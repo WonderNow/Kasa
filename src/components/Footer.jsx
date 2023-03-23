1dd58b2ad1fa037b31d90a152403e679f2ecaf35
Tom Gasche <tomgasche@hotmail.com>
2023-03-23 16:29:23 +0100

    Configuration de ESLint et Prettier

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,11 +3,12 @@ import logo from '../assets/logo-footer.png';
 function footer() {
   return (
     <footer className="footer">
-      <a href='#'><img src={logo} alt="Logo de Kasa" className='kasa-logo' /></a>
+      <a href="#">
+        <img src={logo} alt="Logo de Kasa" className="kasa-logo" />
+      </a>
       <p>© 2023 Kasa. All rights reserved</p>
     </footer>
-
-  )
+  );
 }
 
 export default footer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,15 +4,18 @@ function header() {
   return (
     <header className="header">
       <nav>
-        <img src={logo} alt="Logo de Kasa" className='kasa-logo' />
+        <img src={logo} alt="Logo de Kasa" className="kasa-logo" />
         <ul>
-          <li className='underline-1'><a href='#'>Accueil</a></li>
-          <li className='underline-2'><a href='#'>À Propos</a></li>
+          <li className="underline-1">
+            <a href="#">Accueil</a>
+          </li>
+          <li className="underline-2">
+            <a href="#">À Propos</a>
+          </li>
         </ul>
       </nav>
     </header>
-
-  )
+  );
 }
 
 export default header;
